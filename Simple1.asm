e00feeffa70b0bf38bcb70580474a1062fcf8c7e
isb17 <isb17@skbl-422-009.ph.ic.ac.uk>
2019-10-28 16:59:21 +0000

    multiplication

--- Simple1.asm
+++ Simple1.asm
@@ -4,6 +4,9 @@
 	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
 	extern	LCD_Write_Hex			    ; external LCD subroutines
 	extern  ADC_Setup, ADC_Read		    ; external ADC routines
+	extern  keyboard_setup
+	
+
 	
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
@@ -19,6 +22,11 @@ pdata	code    ; a section of programme memory for storing data
 	; ******* myTable, data in programme memory, and its length *****
 myTable data	    "Hello World!\n"	; message, plus carriage return
 	constant    myTable_l=.13	; length of data
+	constant    SixteenBit = 0xFED2 ; Sixteen bit number for multiplication
+	constant    SixteenBit1 = 0xC448
+
+;acs0	udata_acs 	
+EightBit    res 1 ; reserve 1 byte for 8 bit number	
 	
 main	code
 	; ******* Programme FLASH read Setup Code ***********************
@@ -27,10 +35,15 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup LCD
 	call	ADC_Setup	; setup ADC
+	call	keyboard_setup
 	goto	start
 	
 	; ******* Main programme ****************************************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
+start 	;movlw	0XFF
+	;movwf	EightBit	;multiply by FF
+	;call	Eight_Sixteen
+	call	Sixteen_Sixteen
+	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
 	movlw	upper(myTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
@@ -52,6 +65,13 @@ loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	lfsr	FSR2, myArray
 	call	UART_Transmit_Message
 	
+;	movlw   high(SixteenBit) ;ForEight_Sixteen routine
+;	movwf	0x20
+;	movlw	low(SixteenBit)
+;	movwf	0x21
+;	movlw	EightBit
+;	movwf	0x30
+	
 measure_loop
 	call	ADC_Read
 	movf	ADRESH,W
@@ -65,4 +85,58 @@ delay	decfsz	delay_count	; decrement until zero
 	bra delay
 	return
 
+Eight_Sixteen
+    movlw 0xFF
+    movwf 0x50		;file for later comparison
+    ;movf EightBit, W
+    ;movwf 0x30		;move to arbitary file for storage
+    movlw low(SixteenBit)
+    mulwf EightBit	
+    movff PRODL, 0x12	;move to arbitary file for storage
+    movff PRODH, 0x14	;move to arbitary file for storage (later add to low half of high product)
+    movlw high(SixteenBit)
+    mulwf EightBit
+    movff PRODL, 0x11	;move to arbitary file for storage (later add to high half of low product)
+    movff PRODH, 0x10	;move to arbitary file for storage
+    movf  0x14, W
+    addwf 0x11
+    movf  0x11, W
+    movlw 0x00
+    addwfc 0x10
+    ;goto fin ;this is fucked (previously return)
+    ;movf  0x10, W
+    ;addlw 0x01
+    ;movwf 0x10
+    ;fin ;didn't previously exist
+    return
+    
+Sixteen_Sixteen
+    movlw low(SixteenBit1)
+    movwf EightBit
+    movff EightBit, 0x30 ;test
+    call Eight_Sixteen
+    movff 0x12, 0x63 ;store final answer in row 6
+    movff 0x11, 0x62 ; middle digit to be added to lowest digit of nect multiplication
+    movff 0x10, 0x61 ; top digit to be added to middle digit of next multiplication
+    movlw high(SixteenBit1)
+    movwf EightBit
+    movff EightBit, 0x31 ;test
+    call Eight_Sixteen
+    movf 0x12, W
+    addwf 0x62
+    movlw 0x00
+    addwfc 0x61
+    movf 0x11, W
+    addwf 0x61 
+    movlw 0x00
+    movwf 0x60
+    addwfc 0x60
+    movf 0x10, W
+    addwf 0x60 
+    return
+    
+   
+    
+ 
+     
 	end
