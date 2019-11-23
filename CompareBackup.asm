d830226ef59417cfff72f378e795a204a3e37b7e
ivanlyw <48099969+ivanlyw@users.noreply.github.com>
2019-11-23 16:20:25 +0000

    Final report
    Tidied and commented

--- CompareBackup.asm
+++ CompareBackup.asm
@@ -1,14 +1,17 @@
 #include p18f87k22.inc
-    global store1, store2, store3, store4, storeKey1, storeKey2, storeKey3, storeKey4
-    extern passcode1, passcode2, passcode3, passcode4
+    global      store1, store2, store3, store4, storeKey1, storeKey2, storeKey3, storeKey4
+    extern      passcode1, passcode2, passcode3, passcode4
     
-acs5	udata_acs
+acs1	udata_acs
 storeKey1  res 1
 storeKey2  res 1
 storeKey3  res 1
 storeKey4  res 1
   
     code
+    
+;Backup routine that doesn't rely on external memory 
+;Each correct passcode character is stored in respective storeKey registers
 
 store1
     movff   passcode1, storeKey1
